e3616484bed5707d40686f5da15e4c31a6c249ed
JamesDavidBielby <66780283+JamesDavidBielby@users.noreply.github.com>
2020-06-11 12:33:35 +0100

    ACI Module

---
+++ FFNetwork.m
@@ -0,0 +1,46 @@
+net = patternnet([10 7]);
+[net,tr] = train(net,x,t);
+plotperform(tr);
+
+y=net(x);
+
+
+[m,n] = size(x);
+count = 0; %overall dataset
+
+for i=1:n
+    y(i) = round(y(i));
+    if y(i)<0.5
+        y(i)=0;
+    end
+    if y(i)>=0.5
+        y(i)=1;
+    end
+    if t(i)==y(i)
+        count=count+1;
+    end
+    
+   
+end
+
+
+disp('Number of Correct Units: ');
+disp(count);
+disp('Number of Incorrect Units: ');
+disp(n- count);
+count=count*100;
+count=count/n;
+disp('% Correct Classification:');
+disp(count); 
+
+[m,n] = size(tr.time);
+
+timelength =tr.time(n);
+
+
+
+timeavg = timelength/n;
+disp('Time: ');
+disp(timelength);
+disp('Avg Time: ');
+disp(timeavg);
